037de6278627461a16e6d9e0782edd7b5939364c
Gabriel <gabrieldavid_98@hotmail.com>
2020-12-26 22:15:13 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wing-it
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wing_it/core_test.clj
@@ -0,0 +1,7 @@
+(ns wing-it.core-test
+  (:require [clojure.test :refer :all]
+            [wing-it.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
